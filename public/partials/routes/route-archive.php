ddaa90bc25dfb420eeaea38622213d3430b8bd34
fabolivar <fabian.altahona@koombea.com>
2019-04-26 06:48:59 -0500

    fixed context-query.php file path on hierarchy Archive

--- public/partials/routes/route-archive.php
+++ public/partials/routes/route-archive.php
@@ -6,7 +6,7 @@
  */
 
 // Include common actions.
-include_once( 'context-query.php' );
+include_once( 'inc/context-query.php' );
 $post_types = array_filter( (array) get_query_var( 'post_type' ) );
 if ( count( $post_types ) === 1 ) {
 	$post_type   = reset( $post_types );
