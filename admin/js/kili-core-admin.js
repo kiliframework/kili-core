76fb964e84058031a1f3c04ec8b4662a53f7ec85
Rhonalf Martinez <rhonalf.martinez@koombea.com>
2019-06-04 17:42:28 -0500

    change script structure and use minified js

--- admin/js/kili-core-admin.js
+++ admin/js/kili-core-admin.js
@@ -1,115 +1,160 @@
-let Kili = {};
-Kili.Basic = (() => {
-  const hiddenClass = 'hidden';
-  let isClassicEditorEnabled = false;
-  let isGutenbergEditorEnabled = false;
-  let isActivated = false;
+let Kili = {
+  apiSettings: null,
+  hiddenClass: 'hidden',
+  isActivated: false,
+  isClassicEditorEnabled: false,
+  isGutenbergEditorEnabled: false,
+  postId: 0,
 
-  const checkActiveEditor = () => {
-    if (document.querySelector('#titlediv')) {
-      isClassicEditorEnabled = true;
+  getApiSettings: () => Kili.apiSettings,
+  getCurrentPostId: () => Kili.postId,
+  init: () => {
+    Kili.setApiSettings();
+    Kili.setCurrentPostId();
+  },
+  setApiSettings: () => {
+    Kili.apiSettings = JSON.parse(JSON.stringify(wpApiSettings));
+    Kili.apiSettings.customApiRoute = 'api/v1/';
+  },
+  setCurrentPostId: () => {
+    const parsedLocation = Kili.utils.parsedLocation();
+    if (parsedLocation && parsedLocation.post) {
+      Kili.postId = parseInt(parsedLocation.post);
+    }
+  }
+};
+
+// Note: KiliStrings is an object containing plugin translations. It is set in plugin admin area.
+
+Kili.Ajax = {
+  checkIfKiliWasActivated: () => {
+    let ajax = fetch('/wp-json/' + Kili.getApiSettings().customApiRoute + 'post-has-kili/' + Kili.getCurrentPostId());
+    ajax
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        Kili.Basic.setKiliStatus(response == 'active');
+        Kili.Basic.changeStatusCheck();
+      });
+  },
+  updatePostMeta: () => {
+    const data = {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: Kili.getCurrentPostId(),
+        value: Kili.Basic.isKiliActive() ? 'active' : 'inactive'
+      })
+    };
+    let ajax = fetch('/wp-json/' + Kili.getApiSettings().customApiRoute + 'set-post-kili/', data);
+    ajax
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        let backgroundColor = response == true ? '2274A5' : 'D33F49';
+        console.log('%c Info ', 'color: white; background-color: #' + backgroundColor + '; border-radius: 4px;', (response == true ? 'Successful' : 'Failed') + ' operation');
+      })
+      .catch((error) => {
+        console.log('%c Error ', 'color: white; background-color: #D33F49; border-radius: 4px;', 'Error updating meta: ' + error);
+      });
+  }
+};
+
+Kili.Basic = {
+  changeStatusCheck: () => {
+    if (!Kili.isActivated) {
       return;
     }
-    isGutenbergEditorEnabled = true;
-  };
-
-  const setKiliStatus = (status) => {
-    isActivated = status;
-  };
-
-  const changeStatusCheck = () => {
-    if (isActivated) {
-      document.querySelector('.js-toggle-kili').checked = isActivated;
-      if (isClassicEditorEnabled) {
-        toggleClassicEditor();
-      } else if (isGutenbergEditorEnabled) {
-        toggleGutenbergEditor();
-      }
-      updateCheckboxUI();
+    document.querySelector('.js-toggle-kili').checked = Kili.isActivated;
+    if (Kili.isClassicEditorEnabled) {
+      Kili.Basic.toggleClassicEditor();
+    } else if (Kili.isGutenbergEditorEnabled) {
+      Kili.Basic.toggleGutenbergEditor();
     }
-  };
-
-  const updateCheckboxUI = () => {
-    if (isActivated) {
+    Kili.Basic.updateCheckboxUI();
+  },
+  checkActiveEditor: () => {
+    if (document.querySelector('#titlediv')) {
+      Kili.isClassicEditorEnabled = true;
+      return;
+    }
+    Kili.isGutenbergEditorEnabled = true;
+  },
+  init: () => {
+    if (!Kili.Basic.isActiveAnyEditor()) {
+      return;
+    }
+    Kili.Basic.checkActiveEditor();
+    Kili.Basic.insertButtonInPostInterface();
+  },
+  insertButtonInPostInterface: () => {
+    let containerSelector = '.edit-post-header__settings';
+    const buttonsHtml = '<div class="' + (Kili.isClassicEditorEnabled ? 'misc-pub-section' : 'components-button') + ' enable-kili-toggle">' +
+      '<label class="enable-kili-toggle__title" for="js-toggle-kili">' + KiliStrings.enableKili + '</label> ' +
+      '<label>' +
+      '<input type="checkbox" id="enable_kili" name="enable_kili" value="1" class="acf-switch-input js-toggle-kili" autocomplete="off">' +
+      '<div class="acf-switch js-kili-switch">' +
+      '<span class="acf-switch-on">' + KiliStrings.yes + '</span>' +
+      '<span class="acf-switch-off">' + KiliStrings.no + '</span>' +
+      '<div class="acf-switch-slider"></div>' +
+      '</div>' +
+      '</label>' +
+      '</div>';
+    if (Kili.isClassicEditorEnabled) {
+      containerSelector = '#misc-publishing-actions';
+    }
+    document.querySelector(containerSelector).insertAdjacentHTML('beforeend', buttonsHtml);
+  },
+  isActiveAnyEditor: () => {
+    const location = Kili.utils.parsedLocation();
+    return typeof wpActiveEditor !== 'undefined' &&
+      typeof location.post !== 'undefined' &&
+      (pagenow === 'page' || pagenow === 'post');
+  },
+  isKiliActive: () => {
+    return Kili.isActivated;
+  },
+  setKiliStatus: (status) => {
+    Kili.isActivated = status;
+  },
+  toggleClassicEditor: () => {
+    document.querySelector('#postdivrich').classList.toggle(Kili.hiddenClass);
+  },
+  toggleEditor: (ev) => {
+    if (!Kili.Basic.isActiveAnyEditor()) {
+      return;
+    } else if (Kili.isClassicEditorEnabled) {
+      Kili.Basic.toggleClassicEditor();
+    } else if (Kili.isGutenbergEditorEnabled) {
+      Kili.Basic.toggleGutenbergEditor();
+    }
+    Kili.isActivated = ev.target.checked;
+    Kili.Ajax.updatePostMeta();
+    Kili.Basic.updateCheckboxUI();
+  },
+  toggleGutenbergEditor: () => {
+    document.querySelector('.edit-post-visual-editor').classList.toggle(Kili.hiddenClass);
+  },
+  updateCheckboxUI: () => {
+    if (Kili.isActivated) {
       document.querySelector('.js-kili-switch').classList.add('-on');
       return;
     }
     document.querySelector('.js-kili-switch').classList.remove('-on');
-  };
+  },
+};
 
-  const isActiveAnyEditor = () => {
-    const location = Kili.utils.parsedLocation();
-    return typeof wpActiveEditor !== "undefined" &&
-      typeof location.post !== 'undefined' &&
-      (pagenow === 'page' || pagenow === 'post');
-  };
-
-  const insertButtonInPostInterface = () => {
-    let containerSelector = '.edit-post-header__settings';
-    const buttonsHtml = '<div class="' + (isClassicEditorEnabled ? 'misc-pub-section' : 'components-button') + ' enable-kili-toggle">' +
-        '<label class="enable-kili-toggle__title" for="js-toggle-kili">' + KiliStrings.enableKili + '</label> ' +
-        '<label>' +
-          '<input type="checkbox" id="enable_kili" name="enable_kili" value="1" class="acf-switch-input js-toggle-kili" autocomplete="off">' +
-          '<div class="acf-switch js-kili-switch">' +
-            '<span class="acf-switch-on">' + KiliStrings.yes + '</span>' +
-            '<span class="acf-switch-off">' + KiliStrings.no + '</span>' +
-            '<div class="acf-switch-slider"></div>' +
-          '</div>' +
-        '</label>' +
-      '</div>';
-    if (isClassicEditorEnabled) {
-      containerSelector = '#misc-publishing-actions';
-    }
-    document.querySelector(containerSelector).insertAdjacentHTML('beforeend', buttonsHtml);
-  };
-
-  const toggleClassicEditor = () => {
-    document.querySelector('#postdivrich').classList.toggle(hiddenClass);
-  };
-
-  const toggleEditor = (ev) => {
-    if (!isActiveAnyEditor()) {
-      return;
-    } else if (isClassicEditorEnabled) {
-      toggleClassicEditor();
-    } else if (isGutenbergEditorEnabled) {
-      toggleGutenbergEditor();
-    }
-    isActivated = ev.target.checked;
-    Kili.Ajax.updatePostMeta();
-    updateCheckboxUI();
-  };
-
-  const toggleGutenbergEditor = () => {
-    document.querySelector('.edit-post-visual-editor').classList.toggle(hiddenClass);
-  };
-
-  const isKiliActive = () => isActivated;
-
-  const init = () => {
-    if (isActiveAnyEditor()) {
-      checkActiveEditor();
-      insertButtonInPostInterface();
-    }
-  };
-
-  return {
-    changeStatusCheck: changeStatusCheck,
-    init: init,
-    isActiveAnyEditor: isActiveAnyEditor,
-    isKiliActive: isKiliActive,
-    setKiliStatus: setKiliStatus,
-    toggleEditor: toggleEditor
-  };
-})();
-
-Kili.utils = (() => {
-  const parsedLocation = () => {
-    let query = location.search;
-    if (typeof query === 'undefined') {
+Kili.utils = {
+  parsedLocation: () => {
+    if (typeof location.search === 'undefined') {
       return;
     }
-    const vars = query.substring(1).split('&');
+    const vars = location.search.substring(1).split('&');
     let queryString = {};
     for (let i = 0; i < vars.length; i++) {
       const pair = vars[i].split('=');
@@ -127,104 +172,17 @@ Kili.utils = (() => {
       queryString[key].push(decodeURIComponent(value));
     }
     return queryString;
-  };
+  }
+};
 
-  return {
-    parsedLocation: parsedLocation
-  };
-})();
-
-Kili.Ajax = (() => {
-  const apiRoute = 'api/v1/';
-  let apiSettings = null;
-  let adminURL = '';
-  let postId = 0;
-
-  const init = () => {
-    setAdminURL();
-    setApiSettings();
-    setCurrentPostId();
-    checkIfKiliWasActivated();
-  };
-
-  const checkIfKiliWasActivated = () => {
-    let ajax = fetch('/wp-json/' + apiRoute + 'post-has-kili/' + getCurrentPostId());
-    ajax
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        Kili.Basic.setKiliStatus(response == 'active');
-        Kili.Basic.changeStatusCheck();
-      });
-  };
-
-  const getAdminURL = () => adminURL;
-
-  const getApiSettings = () => apiSettings;
-
-  const getCurrentPostId = () => postId;
-
-  const setAdminURL = () => {
-    adminURL = wp.ajax.settings.url;
-  };
-
-  const setApiSettings = () => {
-    apiSettings = JSON.parse(JSON.stringify(wpApiSettings));
-    apiSettings.customApiRoute = apiRoute;
-  };
-
-  const setCurrentPostId = () => {
-    const parsedLocation = Kili.utils.parsedLocation();
-    if (parsedLocation && parsedLocation.post) {
-      postId = parseInt(parsedLocation.post);
-    }
-  };
-
-  const updatePostMeta = () => {
-    const data = {
-      method: 'PUT',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: getCurrentPostId(),
-        value: Kili.Basic.isKiliActive() ? 'active' : 'inactive'
-      })
-    };
-    let ajax = fetch('/wp-json/' + getApiSettings().customApiRoute + 'set-post-kili/', data);
-    ajax
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        let backgroundColor = response == true ? '2274A5' : 'D33F49';
-        console.log('%c Info ', 'color: white; background-color: #' + backgroundColor + '; border-radius: 4px;', (response == true ? 'Successful' : 'Failed') + ' operation');
-      })
-      .catch((error) => {
-        console.log('%c Error ', 'color: white; background-color: #D33F49; border-radius: 4px;', 'Error updating meta: ' + error);
-      });
-  };
-
-  return {
-    getAdminURL: getAdminURL,
-    getApiSettings: getApiSettings,
-    getCurrentPostId: getCurrentPostId,
-    init: init,
-    setAdminURL: setAdminURL,
-    setApiSettings: setApiSettings,
-    setCurrentPostId: setCurrentPostId,
-    updatePostMeta: updatePostMeta
-  };
-})();
-
+// Init Kili
 (function ($) {
   'use strict';
   $(window).load(() => {
     Kili.Basic.init();
     if (Kili.Basic.isActiveAnyEditor()) {
-      Kili.Ajax.init();
+      Kili.init();
+      Kili.Ajax.checkIfKiliWasActivated();
     }
   });
   $(document).on('change', '.js-toggle-kili', Kili.Basic.toggleEditor);
